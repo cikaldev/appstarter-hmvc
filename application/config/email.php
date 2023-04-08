98b91f78cb3c3dbfa9ecdd7ba9272fb5003557b0
cikaldev <cikaldev@gmail.com>
2023-04-08 12:50:54 +0700

    add empty config email

---
+++ application/config/email.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Please read documentation about Email Library
+ 
+$config = array(
+  "protocol"    => "smtp",
+  "smtp_host"   => "",
+  "smtp_user"   => "",
+  "smtp_pass"   => "",
+  "smtp_port"   => 587,
+  "smtp_crypto" => "tls",
+  "mailtype"    => "html",
+  "charset"     => "iso-8859-1",
+  "newline"     => "\r\n"
+);
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */
